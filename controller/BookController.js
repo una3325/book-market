69fb2e627768f78e5d40a8197a4a7d98984f2e8a
una3325 <a33256448@gmail.com>
2024-05-27 10:14:15 +0900

    도서 목록 조회 페이징

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,38 +2,41 @@ const conn = require('../mariadb')
 const { StatusCodes } = require('http-status-codes')
 
 const allBooks = (req, res) => {
-  let { category_id } = req.query
+  let { category_id, news, limit, currentPage } = req.query
 
-  if (category_id) {
-    let sql = 'SELECT * FROM books WHERE category_id = ?'
-    conn.query(sql, category_id, (err, results) => {
-      if (err) {
-        console.log(err)
-        return res.status(StatusCodes.BAD_REQUEST).end()
-      }
-      if (results.length) {
-        return res.status(StatusCodes.OK).json(results)
-      } else {
-        return res.status(StatusCodes.NOT_FOUND).end()
-      }
-    })
-  } else {
-    let sql = 'SELECT * FROM books'
-    conn.query(sql, (err, results) => {
-      if (err) {
-        console.log(err)
-        return res.status(StatusCodes.BAD_REQUEST).end()
-      } else {
-        return res.status(StatusCodes.OK).json(results)
-      }
-    })
+  let offset = limit * (currentPage - 1)
+  let sql = 'SELECT * FROM books '
+  let values = []
+  if (category_id && news) {
+    sql += ` WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(),INTERVAL 1 MONTH)AND NOW()`
+    values = [category_id]
+  } else if (category_id) {
+    sql += ' WHERE category_id=?'
+    values = [category_id]
+  } else if (news) {
+    sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(),INTERVAL 1 MONTH)AND NOW()`
   }
+
+  sql += ' LIMIT ? OFFSET ?'
+  values.push(parseInt(limit), offset)
+
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err)
+      return res.status(StatusCodes.BAD_REQUEST).end()
+    }
+    if (results.length) {
+      return res.status(StatusCodes.OK).json(results)
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).end()
+    }
+  })
 }
 
 const bookDetail = (req, res) => {
   let { id } = req.params
 
-  let sql = 'SELECT * FROM books WHERE id = ?'
+  let sql = `SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE books.id = ?`
   conn.query(sql, id, (err, results) => {
     if (err) {
       console.log(err)
