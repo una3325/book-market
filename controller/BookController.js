8e8496fcd6514410b8d843d74c417c8ba18c760c
una3325 <a33256448@gmail.com>
2024-05-29 04:55:05 +0900

    좋아요 기능 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,11 +1,14 @@
 const conn = require('../mariadb')
 const { StatusCodes } = require('http-status-codes')
 
+
 const allBooks = (req, res) => {
   let { category_id, news, limit, currentPage } = req.query
 
+
   let offset = limit * (currentPage - 1)
-  let sql = 'SELECT * FROM books '
+  let sql =
+    'SELECT *,(SELECT count(*) FROM likes WHERE liked_book_id = books.id) AS likes FROM books '
   let values = []
   if (category_id && news) {
     sql += ` WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(),INTERVAL 1 MONTH)AND NOW()`
@@ -34,10 +37,18 @@ const allBooks = (req, res) => {
 }
 
 const bookDetail = (req, res) => {
-  let { id } = req.params
+  let { user_id } = req.body
+  let book_id = req.params.id
 
-  let sql = `SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE books.id = ?`
-  conn.query(sql, id, (err, results) => {
+  let sql = `SELECT *,
+  (SELECT count(*) FROM likes WHERE liked_book_id = books.id) AS likes,
+  (SELECT EXISTS (SELECT * FROM likes WHERE user_id = ? AND liked_book_id = ?)) AS liked
+  FROM books  
+  LEFT JOIN category 
+  ON books.category_id = category.category_id  
+  WHERE books.id = ?;`
+  let values = [user_id, book_id, book_id]
+  conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err)
       return res.status(StatusCodes.BAD_REQUEST).end()
---
+++ routes/likes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const { addLike, removeLike } = require('../controller/likeController')
+
+router.use(express.json())
+
+router.post('/:id', addLike)
+
+router.delete('/:id', removeLike)
+
+module.exports = router
