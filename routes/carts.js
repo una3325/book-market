b87c39dacf1fa3e5ce9c1f21c9d5705e4d25e4f5
una3325 <a33256448@gmail.com>
2024-06-03 04:08:02 +0900

    장바구니 상품 목록 구현

---
+++ routes/carts.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const {
+  addToCart,
+  getCartItems,
+  removeCartItem,
+} = require('../controller/CartController')
+router.use(express.json())
+
+router.post('/', addToCart)
+router.get('/', getCartItems)
+router.delete('/:id', removeCartItem)
+
+module.exports = router
