bcbd865b9fa068946399a72a95ac3f24de110ad3
una3325 <a33256448@gmail.com>
2024-12-11 10:54:22 +0900

    feat : cors 설정 추가

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.listen(process.env.PROT)
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-const userRouter = require('./routes/users')
-const bookRouter = require('./routes/books')
-const categoryRouter = require('./routes/category')
-const likeRouter = require('./routes/likes')
-const cartRouter = require('./routes/carts')
-const orderRouter = require('./routes/orders')
+app.listen(process.env.PROT);
 
-app.use('/users', userRouter)
-app.use('/books', bookRouter)
-app.use('/category', categoryRouter)
-app.use('/likes', likeRouter)
-app.use('/carts', cartRouter)
-app.use('/orders', orderRouter)
+const userRouter = require("./routes/users");
+const bookRouter = require("./routes/books");
+// const categoryRouter = require('./routes/category')
+const likeRouter = require("./routes/likes");
+const cartRouter = require("./routes/carts");
+const orderRouter = require("./routes/orders");
+
+app.use("/users", userRouter);
+app.use("/books", bookRouter);
+// app.use('/category', categoryRouter)
+app.use("/likes", likeRouter);
+app.use("/carts", cartRouter);
+app.use("/orders", orderRouter);
